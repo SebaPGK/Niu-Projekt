bf4aef37394284cb977bde4041cfbc29c2856a17
Konrad1124 <konradurbanski2k@gmail.com>
2023-04-18 14:37:33 +0200

    dodanie projektu

---
+++ Niu/Assets/Scenes/Scripts/PresentationGestures.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PresentationGestures : MonoBehaviour {
+    
+    private ReadingGesture readingGesture;
+
+    // Use this for initialization
+    void Start () {
+        Cursor.visible = false;
+        
+        readingGesture = Camera.main.GetComponent<ReadingGesture>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        KinectManager kinectManager = KinectManager.Instance;
+        Debug.Log("weee");
+    }
+}
