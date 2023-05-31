184d31d65e0ea6c525f8762a2fc8d871a3db2978
Konrad1124 <konradurbanski2k@gmail.com>
2023-05-31 13:07:15 +0200

    added posytion rekognition

--- Niu/Assets/Scenes/Scripts/PresentationGestures.cs
+++ Niu/Assets/Scenes/Scripts/PresentationGestures.cs
@@ -10,8 +10,10 @@ public class PresentationGestures : MonoBehaviour
     public KinectWrapper.NuiSkeletonPositionIndex TrackedJoint3 = KinectWrapper.NuiSkeletonPositionIndex.HandRight;
     public GameObject OverlayObject;
     private ReadingGesture readingGesture;
-    public float smoothFactor = 5f; 
+    public float smoothFactor = 5f;
     private float distanceToCamera = 10f;
+    public bool isExecuted = false;
+    public int[] coordinates = new int[9];
 
     // Use this for initialization
     void Start()
@@ -42,13 +44,13 @@ public class PresentationGestures : MonoBehaviour
                     Vector3 posJoint = manager.GetRawSkeletonJointPos(userId, iJointIndex);
                     if (readingGesture.StartDraw == true)
                     {
-                        Draw();
+                        Draw(posJoint);
                     }
                     if (posJoint != Vector3.zero)
                     {
                         // 3d position to depth
                         Vector2 posDepth = manager.GetDepthMapPosForJointPos(posJoint);
-                        
+
 
                         // depth pos to color pos
                         Vector2 posColor = manager.GetColorMapPosForDepthPos(posDepth);
@@ -56,7 +58,10 @@ public class PresentationGestures : MonoBehaviour
                         float scaleX = (float)posColor.x / KinectWrapper.Constants.ColorImageWidth;
                         float scaleY = 1.0f - (float)posColor.y / KinectWrapper.Constants.ColorImageHeight;
 
-
+                        if (readingGesture.StartDraw == true)
+                        {
+                            Draw(posColor);
+                        }
 
                         if (OverlayObject)
                         {
@@ -69,8 +74,43 @@ public class PresentationGestures : MonoBehaviour
         }
     }
 
-    void Draw()
+    void Draw(Vector2 posColor)
     {
+        if (isExecuted = !true)
+        {
+            int i = 0;
+            coordinates[i]= 3;
+            isExecuted = true;
+            i++;
+            while (isExecuted == true)
+            {
+                Debug.Log(posColor);
+            /*
+                if (posColor.x > 0 && posColor.x< 3 && posColor.y> 0 && posColor.y< 3)
+                {
+                  if(coordinates[i]== coordinates[i-1])
+                    {
+                        continue;
+                    }
+                    coordinates[i] = 1;
+                    i++;
+                }
+                else if (posColor.x > 3 && posColor.x < 6 && posColor.y > 3 && posColor.y < 6)
+                {
+                    if (coordinates[i] == coordinates[i - 1])
+                    {
+                        continue;
+                    }
+                    coordinates[i] = 2;
+                    i++;
+                }
+                else 
+                {
+                    continue;
+                }*/
 
+
+            }
+        }
     }
 }
